f9951c957797fab430a22036f816f7166e607472
TCRF <Adamklassen135@gmail.com>
2026-03-21 15:05:10 -0400

    adam k. test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/adam.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="nate opMode")
-public class  adam extends LinearOpMode {
+public class  adam extends OpMode {
 
     @Override
-    public void runOpMode() {
+   public void init() {
+
+    }
+
+    @Override
+    public void loop() {
         int shayshay = 9;
         int mochi = 3;
         int wise_tree = shayshay + mochi;
