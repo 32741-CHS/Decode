6ce5c55a5dd183cdee6fa217c8cac31812f4c706
SyntaxBreak <88933260+SyntaxBreak@users.noreply.github.com>
2026-06-17 12:41:48 +1000

    change from linear to regular opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
@@ -1,31 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.Chassis;
 
 
-@TeleOp(name="Base Teleop", group="Linear OpMode")
-public class BaseTeleop extends LinearOpMode {
+@TeleOp(name="Base Teleop", group="OpMode")
+public class BaseTeleop extends OpMode {
     private Chassis chassis;
 
     @Override
-    public void runOpMode() {
+    public void init() {
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+    }
 
-        waitForStart();
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+    public void loop() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
 
-            if (gamepad1.right_bumper) {
-                chassis.drive_field(y, x, rx);
-            } else {
-                chassis.drive_robot(y, x, rx);
-            }
+        if (gamepad1.right_bumper) {
+            chassis.drive_field(y, x, rx);
+        } else {
+            chassis.drive_robot(y, x, rx);
         }
-    }}
+    }
+}
