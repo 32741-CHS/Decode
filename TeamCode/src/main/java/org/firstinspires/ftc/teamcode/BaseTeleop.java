1227b1c527bb1b97d831552e34fee0eb32b118cc
SyntaxBreak <88933260+SyntaxBreak@users.noreply.github.com>
2026-06-17 12:08:10 +1000

    add vision detection for goal tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
@@ -4,14 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.Chassis;
+import org.firstinspires.ftc.teamcode.mechanisms.Vision;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 
 @TeleOp(name="Base Teleop", group="OpMode")
 public class BaseTeleop extends OpMode {
-    private Chassis chassis;
+    Chassis chassis = new Chassis();
+    Vision vision = new Vision();
+
+    boolean isRed = false;
 
     @Override
     public void init() {
+        vision.init(hardwareMap, telemetry);
+        chassis.init(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -28,5 +35,9 @@ public class BaseTeleop extends OpMode {
         } else {
             chassis.drive_robot(y, x, rx);
         }
+
+        vision.update();
+        AprilTagDetection goalTag = vision.getTagById(isRed ? 24 : 20);
+        vision.displayTagTelemetry(goalTag);
     }
 }
