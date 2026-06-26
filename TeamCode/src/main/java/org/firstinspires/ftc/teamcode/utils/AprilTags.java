f63085cdf74177b839d59bb4b14f50b2df42282a
Pixlox <85137818+Pixlox@users.noreply.github.com>
2026-06-26 16:37:22 +1000

    Rename Apriltags class to AprilTags (capital T)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AprilTags.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.utils;
 
 // 20 = blue goal, 24 = red goal
-public class Apriltags {
+public class AprilTags {
     public static final int RED_GOAL = 24;
     public static final int BLUE_GOAL = 20;
 }
